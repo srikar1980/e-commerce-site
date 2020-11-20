bd63effa616f575acaa46f5ebb1bf17971155a75
srikar <srikar.ravoori@gmail.com>
2020-11-21 01:14:44 +0530

    checkout form

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util.js';
 export default class Cart extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name:"",
+      email:"",
+      address:"",
+      showCheckout: false};
+  }
+  handleInput = (e) =>{
+    this.setState({[e.target.name]:e.target.value});
+  };
+  createOrder = (e)=>{
+    e.preventDefault();
+    const order = {
+      name:this.state.name,
+      email:this.state.email,
+      address:this.state.address,
+      cartItems:this.props.cartItems,
+    }
+    this.props.createOrder(order);
+  }
     render() {
         const {cartItems} = this.props;
         return (
@@ -23,8 +44,9 @@ export default class Cart extends Component {
                       <div>
                         <div>{item.title}</div>
                         <div className="right">
-                            {formatCurrency(item.price)} x {item.count} {" "}
-                          <button className="button"
+                          {formatCurrency(item.price)} x {item.count}{" "}
+                          <button
+                            className="button"
                             onClick={() => this.props.removeFromCart(item)}
                           >
                             Remove
@@ -35,24 +57,68 @@ export default class Cart extends Component {
                   ))}
                 </ul>
               </div>
-              {cartItems.length!==0 && (
+              {cartItems.length !== 0 && (
+                <div>
                   <div className="cart">
                     <div className="total">
-                  <div>
-                      Total:{" "}
-                      {formatCurrency(
-                      cartItems.reduce((a,c)=> a + c.price * c.count,0)
-                   ) }
-
+                      <div>
+                        Total:{" "}
+                        {formatCurrency(
+                          cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                        )}
                       </div>
-                      <button className="button primary">
-                          Proceed
+                      <button
+                        onClick={() => {
+                          this.setState({ showCheckout: true });
+                        }}
+                        className="button primary"
+                      >
+                        Proceed
                       </button>
                     </div>
-              </div>
-
+                  </div>
+                  {this.state.showCheckout && (
+                    <div className="cart">
+                      <form onSubmit={this.createOrder}>
+                        <ul className="form-container">
+                          <li>
+                            <label>Email</label>
+                            <input
+                              name="email"
+                              type="email"
+                              required
+                              onChange={this.handleInput}
+                            ></input>
+                          </li>
+                          <li>
+                            <label>Name</label>
+                            <input
+                            name="name"
+                              type="text"
+                              required
+                              onChange={this.handleInput}
+                            ></input>
+                          </li>
+                          <li>
+                            <label>Address</label>
+                            <input
+                              name="address"
+                              type="text"
+                              required
+                              onChange={this.handleInput}
+                            ></input>
+                          </li>
+                          <li>
+                            <button className="button primary"   type="submit">
+                              Check Out
+                            </button>
+                          </li>
+                        </ul>
+                      </form>
+                    </div>
+                  )}
+                </div>
               )}
-              
             </div>
           </div>
         );
